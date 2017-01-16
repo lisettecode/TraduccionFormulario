9b4133b03c3bc577942b35f49f3c854659f8ae1d
Liichy Aguilar Bueno <Liichy Aguilar Bueno>
2017-01-15 22:43:27 -0500

    Traduccíón en JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function translate(){
+
+	var sesión = document.getElementById('form-signin-heading');
+	titulo.innerHTML="Por favor, inicia sesion";
+
+	var correo = document.getElementById('inputEmail');
+	correo.placeholder="Por favor ingresa tu correo";
+
+	var contraseña = document.getElementById('inputPassword');
+	clave.placeholder="Contraseña";
+
+	var guardar = document.querySelector('.checkbox span');
+	recordar.innerHTML="Recuérdame";
+
+	var ingresar = document.querySelector('button')[0];
+        iniciar.innerHTML = "Iniciar Sesión";
+
+}
